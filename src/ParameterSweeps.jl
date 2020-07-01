06074b9f6f8d0773cde7c1d4907fd559a21564c3
edwin <ngedwin98@gmail.com>
2020-06-30 18:26:57 -0700

    Fix eval to work on Main scope

--- src/ParameterSweeps.jl
+++ src/ParameterSweeps.jl
@@ -1,13 +1,17 @@
 module ParameterSweeps
 
+using JSON
+
 function load_params(params_dict::Dict{Symbol,<:Number})
-    length(ARGS) > 0 && open(path.join(ARGS[1],"params.json"), "r") do f
+    length(ARGS) > 0 && open(joinpath(ARGS[1],"params.json"), "r") do f
         json_dict = JSON.parse(f)
         for k in keys(params_dict)
             params_dict[k] = json_dict[String(k)]
         end
     end
-    eval.(:($k = $v) for (k,v) in params_dict)
+    for (k,v) in params_dict
+        Core.eval(Main, :($k = $v))
+    end
     return params_dict
 end
 
